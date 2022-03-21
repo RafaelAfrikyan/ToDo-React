958790b0e2324157aca306bef1accc30e37c768a
RafaelAfrikyan <rafaelafrikyan95@gmail.com>
2022-03-21 16:59:34 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,64 +1,35 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./App.css";
-import ToDoFooter from "./ToDoFooter";
-import ToDoForm from "./ToDoForm";
-import ToDoList from "./toDoList";
 
 function App() {
-  const [todos, setTodos] = useState([
-    {
-      id: Math.random(),
-      text: "Learn JS",
-      isCompleted: false,
-    },
-    {
-      id: Math.random(),
-      text: "Learn CSS",
-      isCompleted: false,
-    },
-    {
-      id: Math.random(),
-      text: "Learn React",
-      isCompleted: false,
-    },
-  ]);
+  const [value, setValue] = useState([]);
+  const [result, setResult] = useState([]);
+  const [deleted, setDeleted] = useState([]);
 
+  useEffect(() => {
+    setResult([...value]);
+  }, [value]);
+
+  const CtrlZ = () => {
+    setDeleted([...deleted, result.pop()]);
+    setResult([...result]);
+  };
+  const CtrlShiftZ = () => {
+    setResult([...result, deleted.pop()]);
+  };
+
+  console.log(deleted);
   return (
     <div className="App">
-      <ToDoForm
-        onAdd={(text) => {
-          setTodos([
-            ...todos,
-            {
-              id: Math.random(),
-              text: text,
-              isCompleted: false,
-            },
-          ]);
-        }}
-      />
-      <ToDoList
-        todos={todos}
-        onChange={(newToDo) => {
-          setTodos(
-            todos.map((todo) => {
-              if (todo.id === newToDo.id) {
-                return newToDo;
-              }
-              return todo;
-            })
-          );
-        }}
-        onDelete={(todo) => {
-          setTodos(todos.filter((t) => t.id !== todo.id));
-        }}
-      />
-      <ToDoFooter
-        todos={todos}
-        onComleted={() => {
-          setTodos(todos.filter((todo) => !todo.isCompleted));
-        }}
-      />
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      ></input>
+      <h2>Value : {result}</h2>
+
+      <button onClick={CtrlZ}>Ctrl+Z</button>
+      <button onClick={CtrlShiftZ}>Ctrl + Shift + Z</button>
     </div>
   );
 }
